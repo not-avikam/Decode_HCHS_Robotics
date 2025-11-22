8819678bc1f76caa53991b68fcf505023585cd4f
not-avikam <notavikambali@gmail.com>
2025-11-22 11:36:47 -0600

    ts for you alex

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dttest.java
@@ -0,0 +1,254 @@
+/*   MIT License
+ *   Copyright (c) [2025] [Base 10 Assets, LLC]
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+
+ *   The above copyright notice and this permission notice shall be included in all
+ *   copies or substantial portions of the Software.
+
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *   SOFTWARE.
+ */
+
+
+package org.firstinspires.ftc.teamcode;
+
+import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
+
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.HeadingInterpolator;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
+import org.firstinspires.ftc.vision.opencv.ColorRange;
+import org.firstinspires.ftc.vision.opencv.ImageRegion;
+
+/*
+ * This file includes a teleop (driver-controlled) file for the goBILDA® Robot in 3 Days for the
+ * 2025-2026 FIRST® Tech Challenge season DECODE™!
+ */
+
+
+@TeleOp(name = "dt test", group = "StarterBot")
+//@Disabled
+public class dttest extends OpMode {
+    final double STOP_SPEED = 0.0; //We send this power to the servos when we want them to stop.
+    private Servo pitch = null;
+
+    // Declare OpMode members.
+    private DcMotor leftFrontDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightBackDrive = null;
+    private DcMotorEx launcher = null;
+    private DcMotorEx intake = null;
+    private CRServo agigtator = null;
+
+    private enum IntakeState {
+        ON,
+        OFF;
+    }
+
+    private PathChain score;
+    private final Pose scoreFace = new Pose(137,142);
+    private IntakeState intakeState = IntakeState.OFF;
+    private Follower follower;
+    public Pose startingPose; //See ExampleAuto to understand how to use this
+    private final Pose goalPose = new Pose(132, 134);
+    private TelemetryManager telemetryM;
+    private boolean slowMode = false;
+    private double slowModeMultiplier = 0.5;
+    private enum agigtateState {
+        ON,
+        OFF;
+    }
+
+    private agigtateState agigtatorState;
+    ElapsedTime agigtateTimer = new ElapsedTime();
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        ColorBlobLocatorProcessor colorLocator = new ColorBlobLocatorProcessor.Builder()
+                .setTargetColorRange(ColorRange.ARTIFACT_GREEN)
+                .setTargetColorRange(ColorRange.ARTIFACT_PURPLE)// use a predefined color match
+                .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)    // exclude blobs inside blobs
+                .setRoi(ImageRegion.asUnityCenterCoordinates(-0.5, 0.5, 0.5, -0.5))  // search central 1/4 of camera view
+                .setDrawContours(true)                        // Show contours on the Stream Preview
+                .setBlurSize(5)                               // Smooth the transitions between different colors in image
+                .build();
+
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "frontLeft");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "frontRight");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "backLeft");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "backRight");
+        //launcher = hardwareMap.get(DcMotorEx.class, "launcher");
+        //intake = hardwareMap.get(DcMotorEx.class, "intake");
+        //agigtator = hardwareMap.get(CRServo.class, "agigtator");
+        //pitch = hardwareMap.get(Servo.class, "pitch");
+
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(startingPose == null ? new Pose() : startingPose);
+        follower.update();
+        telemetryM = PanelsTelemetry.INSTANCE.getTelemetry();
+
+        agigtatorState = agigtateState.ON;
+
+        launcher.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        intake.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        launcher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        /*
+         * Setting zeroPowerBehavior to BRAKE enables a "brake mode". This causes the motor to
+         * slow down much faster when it is coasting. This creates a much more controllable
+         * drivetrain. As the robot stops much quicker.
+         */
+        leftFrontDrive.setZeroPowerBehavior(BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(BRAKE);
+        leftBackDrive.setZeroPowerBehavior(BRAKE);
+        rightBackDrive.setZeroPowerBehavior(BRAKE);
+        launcher.setZeroPowerBehavior(BRAKE);
+        intake.setZeroPowerBehavior(BRAKE);
+
+        agigtator.setPower(STOP_SPEED);
+
+        launcher.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(300, 0, 0, 10));
+
+        telemetry.addData("Status", "Initialized");
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit START
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits START
+     */
+    @Override
+    public void start() {
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits START but before they hit STOP
+     */
+    @Override
+    public void loop() {
+
+        // TODO: tune these
+        double distanceToTarget = Math.sqrt(
+                Math.pow(follower.getPose().getX() - 137, 2) +
+                        Math.pow(follower.getPose().getY() - 142, 2)
+        );
+        double goalHeightDifference = 54;
+
+        double launchAngle = Math.acos(goalHeightDifference/distanceToTarget);
+
+        telemetry.addData("Launch Angle", launchAngle);
+
+        pitch.setPosition(launchAngle/300);
+
+
+        follower.update();
+        telemetryM.update();
+        if (!slowMode) follower.setTeleOpDrive(
+                -gamepad1.left_stick_y,
+                -gamepad1.left_stick_x,
+                -gamepad1.right_stick_x,
+                false // Robot Centric
+        );
+            //This is how it looks with slowMode on
+        else follower.setTeleOpDrive(
+                -gamepad1.left_stick_y * slowModeMultiplier,
+                -gamepad1.left_stick_x * slowModeMultiplier,
+                -gamepad1.right_stick_x * slowModeMultiplier,
+                false // Robot Centric
+        );
+
+        if (gamepad1.xWasPressed()) {
+            slowMode = true;
+            slowModeMultiplier += 0.5;
+        } else if (gamepad1.xWasReleased()) {
+            slowMode = false;
+        }
+
+        telemetryM.debug("position", follower.getPose());
+        telemetryM.debug("velocity", follower.getVelocity());
+
+        /*
+         * Here we give the user control of the speed of the launcher motor without automatically
+         * queuing a shot.
+         */
+        if (gamepad1.y) {
+            launcher.setPower(1);
+        } else if (gamepad1.b) { // stop flywheel
+            launcher.setPower(0);
+        }
+
+        if (launcher.getPower() == 1) {
+            gamepad1.rumble(1000);
+        }
+
+        if (gamepad1.aWasPressed()) {
+            switch (intakeState) {
+                case ON:
+                    intakeState = IntakeState.OFF;
+                    intake.setPower(0);
+                    break;
+                case OFF:
+                    intakeState = IntakeState.ON;
+                    intake.setPower(1);
+                    break;
+            }
+
+            if (gamepad1.dpadUpWasPressed()) {
+                agigtator.setPower(1);
+            } else {
+                agigtator.setPower(0);
+            }
+
+        }
+
+        score = follower.pathBuilder()
+                .addPath(new BezierLine(follower.getPose(), follower.getPose()))
+                .setHeadingInterpolation(HeadingInterpolator.facingPoint(scoreFace))
+                .build();
+
+        if (gamepad1.left_trigger != 0) {
+            follower.followPath(score);
+        }
+    }
+
+}
+
+
