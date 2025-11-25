24e98bb3d795782eefdc4c7beb1da2cd9646e002
not-avikam <notavikambali@gmail.com>
2025-11-25 00:23:32 -0600

    Aligned with correct group

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dttest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dttest.java
@@ -63,7 +63,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@TeleOp(name="dt test", group="Starter Bot")
+@TeleOp(name="dt test", group="Testing")
 public class dttest extends LinearOpMode {
 
     // Declare OpMode members for each of the 4 motors.
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intaketest.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+@Configurable
+@TeleOp(name = "intake test", group = "Testing")
+public class intaketest extends OpMode {
+    private DcMotorEx motor = null;
+    private Servo servo = null;
+    private CRServo crservo = null;
+    public static double power = 0;
+    private TelemetryManager panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
+
+
+    @Override
+    public void init() {
+
+        motor = hardwareMap.get(DcMotorEx.class, "motor");
+        servo = hardwareMap.get(Servo.class, "servo");
+        crservo = hardwareMap.get(CRServo.class, "crservo");
+
+        motor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+    }
+    @Override
+    public void loop() {
+        motor.setPower(power);
+        crservo.setPower(gamepad1.left_stick_x);
+
+        if (gamepad1.dpadUpWasPressed()) {
+            servo.setPosition(0);
+        } else if (gamepad1.dpadDownWasPressed()){
+            servo.setPosition(1);
+        }
+
+        panelsTelemetry.addData("motor velocity", motor.getVelocity());
+
+        panelsTelemetry.update(telemetry);
+
+    }
+}
