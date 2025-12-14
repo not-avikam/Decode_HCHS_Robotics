8c9412892cb491e8d6bf7ca7988386f2266ed1d6
not-avikam <notavikambali@gmail.com>
2025-12-14 00:55:14 -0600

    Removed yaw servo control from AprilTagAS

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagAS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagAS.java
@@ -148,9 +148,6 @@ public class AprilTagAS extends LinearOpMode {
         //visionPortal.setProcessorEnabled(aprilTag, true);
 
 
-        yaw  = hardwareMap.get(Servo.class, "yaw");
-
-
         // Wait for the DS start button to be touched.
         telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
         telemetry.addData(">", "Touch START to start OpMode");
@@ -173,7 +170,6 @@ public class AprilTagAS extends LinearOpMode {
                         telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
                         telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
                         telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
-                        yaw.setPosition(detection.ftcPose.yaw/300);
                     } else {
                         telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
                         telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
