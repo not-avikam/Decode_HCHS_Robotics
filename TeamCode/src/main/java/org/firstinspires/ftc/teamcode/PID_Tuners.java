2f7a4486059ff11a84e4f56dcb5406479adeafbd
not-avikam <notavikambali@gmail.com>
2025-12-28 16:23:44 -0600

    Refactored launcher motor and added automatic velocity calculation
    The launcher motor was changed from `DcMotorEx` to the custom `MotorEx` wrapper.
    
    Additionally, this commit introduces automatic launcher velocity calculation based on the distance to the target and the launch angle. The required velocity in inches per second (IPS) is calculated using a physics formula and then converted to ticks per second (TPS) for the motor controller. Telemetry was updated to display these new calculated values.
    
    Support was also added for controlling an agitator servo with the gamepad's right trigger.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID_Tuners.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID_Tuners.java
@@ -23,10 +23,11 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.seattlesolvers.solverslib.hardware.motors.CRServoEx;
+import com.seattlesolvers.solverslib.hardware.motors.MotorEx;
 
 
 @Configurable
-@TeleOp(name = "test", group = "Testing")
+@TeleOp(name = "pid_tuning", group = "Testing")
 public class PID_Tuners extends OpMode {
     public static double kp = 1;
     public static double ki = 1;
@@ -34,8 +35,15 @@ public class PID_Tuners extends OpMode {
     public static double kf = 0;
     public static double velocity = 0;
     public static double velocity_servo = 0;
-    private DcMotorEx motor = null;
+    public static double distanceToTarget = 60; // inches
+    public static double launchAngleDeg = 45;   // degrees
+    double velocityIPS = 0;
+    double velocityTPS = 0;
+    private MotorEx launcher;
     //private Servo servo = null;
+
+    private Servo agigtator;
+
     private CRServoEx crservo1 = null;
     private CRServoEx crservo2 = null;
     private TelemetryManager panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
@@ -44,20 +52,22 @@ public class PID_Tuners extends OpMode {
     @Override
     public void init() {
 
-        motor = hardwareMap.get(DcMotorEx.class, "launcher");
+        launcher = new MotorEx(hardwareMap, "launcher");
         //servo = hardwareMap.get(Servo.class, "servo");
         crservo1 = new CRServoEx(hardwareMap, "yaw1");
         crservo2 = new CRServoEx(hardwareMap, "yaw2");
+        agigtator = hardwareMap.get(Servo.class, "agigtator");
+
 
         crservo1.setPIDF(new PIDFCoefficients(kp, ki, kd, kf));
         crservo2.setPIDF(new PIDFCoefficients(kp, ki, kd, kf));
 
-        motor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        launcher.setZeroPowerBehavior(MotorEx.ZeroPowerBehavior.BRAKE);
     }
     @Override
     public void loop() {
-        motor.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(kp, ki, kd, kf));
-        motor.setVelocity(velocity);
+        launcher.setVeloCoefficients(kp, ki, kd);
+        launcher.setVelocity(velocity);
         crservo1.set(velocity_servo);
         crservo2.set(-velocity_servo);
 
@@ -71,8 +81,45 @@ public class PID_Tuners extends OpMode {
 
          */
 
-        panelsTelemetry.addData("motor velocity", motor.getVelocity());
+        double theta = Math.toRadians(launchAngleDeg);
 
+        // Physics denominator
+        double denom =
+                2 * Math.pow(Math.cos(theta), 2) *
+                        (distanceToTarget * Math.tan(theta) - 39);
+
+        // Compute required IPS safely
+        if (denom > 0 && distanceToTarget > 0) {
+            velocityIPS = Math.sqrt(
+                    (386.4 * distanceToTarget * distanceToTarget) / denom
+            );
+        } else {
+            velocityIPS = 0;
+        }
+
+        // IPS -> TPS (your regression, inverted)
+        if (velocityIPS > 0) {
+            velocityTPS = Math.log(velocityIPS / 69.9) / 0.000821;
+        } else {
+            velocityTPS = 0;
+        }
+
+        // Agitator
+        if (gamepad1.right_trigger != 0) {
+            agigtator.setPosition(0);
+        } else {
+            agigtator.setPosition(0.3);
+        }
+
+
+        // Telemetry
+        panelsTelemetry.addData("Distance (in)", distanceToTarget);
+        panelsTelemetry.addData("Velocity IPS", velocityIPS);
+        panelsTelemetry.addData("Velocity TPS", velocityTPS);
+        panelsTelemetry.addData("Launcher Actual TPS", launcher.getVelocity());
+        panelsTelemetry.update(telemetry);
+
+        telemetry.update();
         panelsTelemetry.update(telemetry);
 
     }
