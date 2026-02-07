9139798898d80ae7fbbaa9c95794495d3a08f080
not-avikam <notavikambali@gmail.com>
2026-02-06 23:06:54 -0600

    Updated pathing and localization constants
    Adjusted the SparkFun OTOS sensor's offset from `(-8.5, 2.75, 2PI)` to `(9, 3, PI)`.
    
    Updated follower constants:
    *   Changed mass from 9.97 to 8.
    *   Added zero power acceleration values for forward (-63.105) and lateral (-92.042) movement.
    *   Set translational PIDF coefficients to (0.3, 0, 0, 0).
    *   Set heading PIDF coefficients to (1, 0, 0, 0.01).
    
    Updated drive constants:
    *   Added X velocity (44.328) and Y velocity (39.973).
    
    Updated OTOS localizer constants:
    *   Changed linear scalar from 21.831 to 1.03.
    *   Changed angular scalar from 0.981 to 1.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.pedroPathing;
 
 import static java.lang.Math.PI;
 
+import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
@@ -17,10 +18,14 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 
 public class Constants {
-    static SparkFunOTOS.Pose2D otos = new SparkFunOTOS.Pose2D(-8.5, 2.75, PI*2);
+    static SparkFunOTOS.Pose2D otos = new SparkFunOTOS.Pose2D(9, 3, PI);
 
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(9.97);
+            .mass(8)
+            .forwardZeroPowerAcceleration(-63.105)
+            .lateralZeroPowerAcceleration(-92.042)
+            .translationalPIDFCoefficients(new PIDFCoefficients(.3,0 ,0,0))
+            .headingPIDFCoefficients(new PIDFCoefficients(1,0 ,0,0.01));;
 
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
@@ -31,14 +36,15 @@ public class Constants {
             .leftFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE);
-
+            .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .xVelocity(44.328)
+            .yVelocity(39.973);
     public static OTOSConstants localizerConstants = new OTOSConstants()
             .hardwareMapName("otos")
             .linearUnit(DistanceUnit.INCH)
             .offset(otos)
-            .linearScalar(21.83100)
-            .angularScalar(.981)
+            .linearScalar(1.03)
+            .angularScalar(1)
             .angleUnit(AngleUnit.RADIANS);
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
