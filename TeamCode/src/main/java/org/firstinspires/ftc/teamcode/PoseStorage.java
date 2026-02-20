b9cb899af095dada0144f33c7f882d942daa0dee
not-avikam <notavikambali@gmail.com>
2026-02-19 22:05:35 -0600

    refactor: Remove unused imports from PoseStorage
    Removed unused imports for `FTCCoordinates`, `PedroCoordinates`, and `AngleUnit` from `PoseStorage.java`. The code was also formatted to standard style.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PoseStorage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PoseStorage.java
@@ -1,3 +1,10 @@
 package org.firstinspires.ftc.teamcode;
+import com.pedropathing.ftc.FTCCoordinates;
+import com.pedropathing.geometry.PedroCoordinates;
 import com.pedropathing.geometry.Pose;
-public class PoseStorage { public static Pose currentPose = null;}
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+public class PoseStorage {
+    public static Pose currentPose = null;
+}
